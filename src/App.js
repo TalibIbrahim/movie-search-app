ade1077947cfa9f3724bbec42c8a0ba94e58be12
TalibIbrahim <talibibrahim04@gmail.com>
2024-03-17 16:44:29 +0500

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NavBar from './components/UI/NavBar';
+import SearchInput from './components/UI/SearhInput';
+import Container from './components/UI/Container';
+
+
 
 function App() {
+
+  const searchHandler = (searchInput) => {
+    console.log(searchInput);
+    console.log('This is being done in the App.js file' , searchInput);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <NavBar />
+      <SearchInput onSearch={searchHandler}  />
+    </Container>
+     
   );
 }
 
---
+++ src/components/UI/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className='font text-7xl py-4 bg-neutral-900'>
+      CINEVERSE
+    </div>
+  );
+}
+
+export default NavBar;
